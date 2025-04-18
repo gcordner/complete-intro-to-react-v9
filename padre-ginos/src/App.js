58e57994940ab9658b131d2374a70b353e117d95
gcordner <geoffcordner@gmail.com>
2025-04-18 07:38:23 -0700

    add jsx

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
-
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
---
+++ padre-ginos/src/Pizza.jsx
@@ -0,0 +1,8 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+export default Pizza;
